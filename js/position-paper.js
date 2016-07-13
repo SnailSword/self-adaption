d019af18b8fa6621082ded84d56cc5307857f12f
SnailSword <saniac@163.com>
2016-07-14 02:08:06 +0800

    改了一下样式

--- js/position-paper.js
+++ js/position-paper.js
@@ -22,14 +22,14 @@ function changeWidth() {
         //     $(this).prev().css("height",thisHeight);
         // });
         $('.dpt-item-multi-line>div').css("width", width - 1);
-        $('.dpt-item-multi-line>div:first-child').css({"height":"40px"});
+        $('.dpt-item-multi-line>div:first-child').css({"height":"40px",'line-height':"40px"});
     }
     else if(width>600){
         $('.dpt-item-b .dpt-item-p1').css("width",width/2-1);
         $('.dpt-item-b .dpt-item-p2').css("width",width/2-1);
         $('.dpt-item-s .dpt-item-p1').css("width",width/4-1);
         $('.dpt-item-s .dpt-item-p2').css("width",width/4-1);
-        $('.dpt-item-multi-line>div:first-child').css({"width":70,"height":"200px"});
+        $('.dpt-item-multi-line>div:first-child').css({"width":70,"height":"200px",'line-height':"200px"});
         $('.dpt-item-multi-line>div:last-child').css("width",width-72);
         // $('.dpt-item-multi-line>div:first-child').css();
     }
