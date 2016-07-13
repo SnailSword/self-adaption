03ee3101a59782deb42dc4e7aa5b0007756f1d38
SnailSword <saniac@163.com>
2016-07-13 22:04:15 +0800

    两个表格的实现 没加样式

---
+++ js/position-paper.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Saniac on 2016/7/13.
+ */
+$(document).ready(function () {
+
+    changeWidth();
+    window.onresize=function(){
+        changeWidth();
+    }
+});
+
+function changeWidth() {
+    var width = $(".div-pretend-table").width();
+    if(width<=600&&width>=300) {
+        $('.dpt-item-p1').css("width", width / 2 - 1);
+        $('.dpt-item-p2').css("width", width / 2 - 1);
+        // $('.dpt-item-multi-line .dpt-item-p2').each(function(){
+        //     var thisHeight=$(this).Height();
+        //     console.log(thisHeight);
+        //     $(this).prev().css("height",thisHeight);
+        // });
+        $('.dpt-item-multi-line>div').css("width", width - 1);
+        $('.dpt-item-multi-line>div:first-child').css({"height":"40px"});
+    }
+    else if(width>600){
+        $('.dpt-item-b .dpt-item-p1').css("width",width/2-1);
+        $('.dpt-item-b .dpt-item-p2').css("width",width/2-1);
+        $('.dpt-item-s .dpt-item-p1').css("width",width/4-1);
+        $('.dpt-item-s .dpt-item-p2').css("width",width/4-1);
+        $('.dpt-item-multi-line>div:first-child').css({"width":70,"height":"200px"});
+        $('.dpt-item-multi-line>div:last-child').css("width",width-72);
+        // $('.dpt-item-multi-line>div:first-child').css();
+    }
+    console.log(width);
+}
---
+++ js/table.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Saniac on 2016/7/13.
+ */
+$(document).ready(function(){
+
+    wordLimit("td:first-child");
+    window.onresize=function(){
+        wordLimit("td:first-child");
+    }
+});
+
+function wordLimit(item) {
+    $(item).each(function(){
+        var maxWidth=$(this).width()/parseInt($(".flex-table").css("font-size"))-2;
+        if($(this).text().length>maxWidth){
+            $(this).text($(this).text().substring(0,maxWidth));
+            $(this).html($(this).html()+'...');
+        }
+    });
+}
